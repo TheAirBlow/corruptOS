d3f9aca75a0858035ccab7f5350b8cd0bd0454de
TheAirBlow <pilexet.danila@gmail.com>
2021-12-14 13:06:11 +0500

    [GOP] Delete non-existion method from header

--- kernel/src/video/gop.h
+++ kernel/src/video/gop.h
@@ -24,8 +24,3 @@ void gop_init(struct stivale2_struct_tag_framebuffer* fb);
 void draw_pixel(uint32_t x, uint32_t y, uint32_t pixel);
 uint32_t get_pixel(uint32_t x, uint32_t y);
 void draw_filled_rectangle(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color);
-void print_character(uint32_t x, uint32_t y, const char chr, uint32_t color);
-void print_position(uint32_t color, char* str, Point* pos);
-void print(uint32_t color, char* str);
-void newline_point(Point* pos);
-void newline();
